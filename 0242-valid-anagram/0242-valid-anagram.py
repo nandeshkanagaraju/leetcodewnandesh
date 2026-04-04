4f21f359732590a654c206ab07590ee13c987b19
Nandesh Kanagaraju <nandeshkanagaraju08@gmail.com>
2026-04-05 01:43:36 +0530

    Time: 14 ms (89.95%), Space: 12.4 MB (91.5%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+        
+        count = {}
+        
+        for c in s:
+            count[c] = count.get(c, 0) + 1
+        
+        for c in t:
+            if c not in count:
+                return False
+            count[c] -= 1
+        
+        return all(v == 0 for v in count.values())
